c8a137667ea4241bc7f56c9fc996579bb5244109
bojorquezja <bojorquez.ja@gmail.com>
2020-01-29 03:51:30 -0500

    falta agregar pantalla de configuracion, la clase utilitaria BD falta probar

--- src/main/java/pe/edu/utp/dao/UtilitarioBD.java
+++ src/main/java/pe/edu/utp/dao/UtilitarioBD.java
@@ -1,5 +1,13 @@
 package pe.edu.utp.dao;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -9,17 +17,21 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.function.BiConsumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class UtilitarioBD {
-    private static final String CONNJDBC = "jdbc:mysql://localhost:3306/ventas1?useLegacyDatetimeCode=false&serverTimezone=America/Lima";
-    private static final String USER = "usuario";
-    private static final String PASS = "contrasena";
+    private static final String CONNJDBC = getJDBCConnection();
+    private static final String USER = getJDBCUser();
+    private static final char[] PASS = getJDBCPassword();
     
     
     public static <T> List<T> traeListaBD(String sql, Class[] tipoObjeto, Object[] valores, BiConsumer<List<T>, ResultSet> accion){
         List<T> tlista = new ArrayList<>();
-        try (Connection con = DriverManager.getConnection(CONNJDBC,USER,PASS);
+        try (Connection con = DriverManager.getConnection(CONNJDBC,USER,PASS.toString());
             PreparedStatement stat = con.prepareStatement(sql);){
             
             for(int x=0 ; x < tipoObjeto.length ; x++){
@@ -52,7 +64,7 @@ public class UtilitarioBD {
     public static boolean grabaTransaccionBD(String[] sql, Class[][] tipoObjeto, Object[][] valores){
         boolean resp = false;
         int totReg=0;
-        try (Connection con = DriverManager.getConnection(CONNJDBC,USER,PASS);){
+        try (Connection con = DriverManager.getConnection(CONNJDBC,USER,PASS.toString());){
             con.setAutoCommit(false);
             for (int z=0 ; z<sql.length ; z++){
                 try (PreparedStatement stat = con.prepareStatement(sql[z]);){
@@ -100,6 +112,95 @@ public class UtilitarioBD {
             return new java.sql.Date(fecha.getTime()).toLocalDate();
         }else{
             return null;
+        }
+    }
+    public static String getJDBCConnection() {
+        try (InputStream input = UtilitarioBD.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                resetJDBCConfiguration();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try (InputStream input = UtilitarioBD.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            //load a properties file from class path, inside static method
+            prop.load(input);
+            return "jdbc:mysql://" + prop.getProperty("JDBC.Server") + ":"+
+                    prop.getProperty("JDBC.Port") + "/" +
+                    prop.getProperty("JDBC.DataBase") +
+                    (prop.getProperty("JDBC.Parameters").isEmpty() ? "" : "?" + prop.getProperty("JDBC.Parameters"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    public static String getJDBCUser() {
+        try (InputStream input = UtilitarioBD.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                resetJDBCConfiguration();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try (InputStream input = UtilitarioBD.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            //load a properties file from class path, inside static method
+            prop.load(input);
+            return prop.getProperty("JDBC.User");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    public static char[] getJDBCPassword() {
+        try (InputStream input = UtilitarioBD.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                resetJDBCConfiguration();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try (InputStream input = UtilitarioBD.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            //load a properties file from class path, inside static method
+            prop.load(input);
+            return prop.getProperty("JDBC.Password").toCharArray();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+    public static void setJDBCConfiguration(String server, String port, String dataBase, String parameters, String user, char[] password) {
+        try (OutputStream output = Files.newOutputStream(Paths.get(UtilitarioBD.class.getClassLoader().getResource("config.properties").toURI()))) {
+            Properties prop = new Properties();
+            // set the properties value
+            prop.setProperty("JDBC.Server", server);
+            prop.setProperty("JDBC.Port", port);
+            prop.setProperty("JDBC.DataBase", dataBase);
+            prop.setProperty("JDBC.Parameters", parameters);
+            prop.setProperty("JDBC.User", user);
+            prop.setProperty("JDBC.Password", password.toString());
+            // save properties to project root folder
+            prop.store(output, null);
+        } catch (IOException | URISyntaxException io) {
+            io.printStackTrace();
+        }
+    }
+    public static void resetJDBCConfiguration() {
+        try (OutputStream output = Files.newOutputStream(Paths.get(UtilitarioBD.class.getClassLoader().getResource("config.properties").toURI()))) {
+            Properties prop = new Properties();
+            // set the properties value
+            prop.setProperty("JDBC.Server", "localhost");
+            prop.setProperty("JDBC.Port", "3306");
+            prop.setProperty("JDBC.DataBase", "ventas1");
+            prop.setProperty("JDBC.Parameters", "useLegacyDatetimeCode=false&serverTimezone=America/Lima");
+            prop.setProperty("JDBC.User", "root");
+            prop.setProperty("JDBC.Password", "");
+            // save properties to project root folder
+            prop.store(output, null);
+        } catch (IOException | URISyntaxException io) {
+            io.printStackTrace();
         }
     }
 }
---
+++ src/main/java/pe/edu/utp/presenter/ConfiguracionPresenter.java
@@ -0,0 +1,69 @@
+package pe.edu.utp.presenter;
+
+import pe.edu.utp.entity.CabGuiaRem;
+import pe.edu.utp.entity.DetGuiaRem;
+import pe.edu.utp.model.MVPModel;
+import pe.edu.utp.view.MVPView;
+
+public class ConfiguracionPresenter implements MVPPresenter{
+    private MVPView view;
+    private MVPModel model;
+    private Object[] result;
+    private String tipoView;
+
+    public ConfiguracionPresenter(MVPView view, MVPModel model, Object[] params) {
+        //tipoview, pk CabGR
+        this.model = model;
+        this.view = view;
+        this.result = new Object[]{(Boolean) true};
+        this.tipoView = (((String) params[0]).length()>=0) ? (String) params[0] : "READ";
+        view.setPresenter(this);
+        CabGuiaRem ent=null;
+        if ( this.tipoView.equalsIgnoreCase("READ") || this.tipoView.equalsIgnoreCase("UPDATE") ){
+            ent = (CabGuiaRem) model.loadModel("CabDet", new Object[]{params[1]})[0];
+        }
+        view.updateView("Iniciar", new Object[]{"Guia de Remision", tipoView, ent});
+        view.showView();
+    }
+    
+    @Override
+    public void setView(MVPView view) {
+        this.view = view;
+    }
+
+    @Override
+    public void setModel(MVPModel model) {
+        this.model = model;
+    }
+
+    @Override
+    public void notifyPresenter(String subject, Object[] params) {
+        if (subject.equalsIgnoreCase("Cancelar")) {
+            view.closeView();
+        }
+        
+        if (subject.equalsIgnoreCase("Aceptar")) {
+            //params: CabGuiaRem con Det
+            if (this.tipoView.equalsIgnoreCase("INSERT")){
+                CabGuiaRem ent = (CabGuiaRem) params[0];
+                model.updateModel("InsertCabDet", new Object[]{ ent });
+                result = new Object[]{(Boolean) true};
+                view.closeView();
+            }
+            if (this.tipoView.equalsIgnoreCase("UPDATE")){
+                CabGuiaRem ent = (CabGuiaRem) params[0];
+                model.updateModel("UpdateCabDet", new Object[]{ ent });
+                result = new Object[]{(Boolean) true};
+                view.closeView();
+            }
+            
+        }
+        
+    }
+
+    @Override
+    public Object[] getResult() {
+        return result;
+    }
+    
+}
