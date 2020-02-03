89c453e0c43fc1678e8362f28715eeecaefa7bc8
bojorquez.ja <bojorquez.ja@gmail.com>
2020-02-03 12:57:52 -0500

    actualizacion de sqls 1,2,3,4

---
+++ src/main/resources/BDVentas1_2.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE ventas1_2 /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+CREATE TABLE cabguiarem (
+  codGuiaRem varchar(12) NOT NULL,
+  fechaEmi date DEFAULT NULL,
+  rucEmpresa varchar(11) DEFAULT NULL,
+  razSocEmpresa varchar(255) DEFAULT NULL,
+  rucCliente varchar(11) DEFAULT NULL,
+  razSocCliente varchar(255) DEFAULT NULL,
+  direcCliente varchar(255) DEFAULT NULL,
+  almacenero varchar(255) DEFAULT NULL,
+  bultos int(11) DEFAULT NULL,
+  PRIMARY KEY (codGuiaRem)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE detguiarem (
+  codGuiaRem varchar(12) NOT NULL,
+  codigoProd varchar(10) DEFAULT NULL,
+  descrProd varchar(255) DEFAULT NULL,
+  cantidad int(11) DEFAULT NULL
+  PRIMARY KEY (codGuiaRem)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE cabfactura (
+  codigoFac varchar(12) NOT NULL,
+  fechaEmi date DEFAULT NULL,
+  codGuiaRem varchar(12) DEFAULT NULL,
+  rucEmpresa varchar(11) DEFAULT NULL,
+  razSocEmpresa varchar(255) DEFAULT NULL,
+  rucCliente varchar(11) DEFAULT NULL,
+  razSocCliente varchar(255) DEFAULT NULL,
+  direcCliente varchar(255) DEFAULT NULL,
+  cajero varchar(255) DEFAULT NULL,
+  subTotal double DEFAULT NULL,
+  igv double DEFAULT NULL,
+  total double DEFAULT NULL,
+  PRIMARY KEY (codigoFac)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE detfactura (
+  codigoFac varchar(12) NOT NULL,
+  codigoProd varchar(10) DEFAULT NULL,
+  descrProd varchar(255) DEFAULT NULL,
+  cantidad int(11) DEFAULT NULL,
+  precUnit double DEFAULT NULL,
+  valorVenta double DEFAULT NULL,
+  PRIMARY KEY (codigoFac)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
